345b4d2d100d1c63fb835426c5c83d69e35599b1
정영준 <jeong-yeongjun@MacBook-Pro-2.local>
2023-01-08 02:44:39 +0900

    2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,60 +5,70 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
-{   
+{
     public Player player;
     public int playerHP;
-  
+    public float gameSpeed = 6f;
+    static float A_y = 7;
+    static float B_y = 0;
+    static float C_y = -7;
+
+
     // public GameObject DomidPannel;
     public GameObject Domidman;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        playerHP=player.HP;
-        if(playerHP==10){
+    {
+        playerHP = player.HP;
+        if (playerHP == 10)
+        {
             caughtByDomidman();
-            player.HP-=1;
-            }
+            player.HP -= 1;
+        }
         Debug.Log(player.HP);
 
-        if(Input.GetKey(KeyCode.R))
-        {   
+        if (Input.GetKey(KeyCode.R))
+        {
             SceneManager.LoadScene(0);
         }
     }
 
 
-    void caughtByDomidman(){
-    Time.timeScale=0f;
-    Debug.Log("CaughtByDomidman"+player.HP);
-    Domidman.SetActive(true);
-    
-        
-}
+    void caughtByDomidman()
+    {
+        Time.timeScale = 0f;
+        Debug.Log("CaughtByDomidman" + player.HP);
+        Domidman.SetActive(true);
 
-    public void ResumeGame(){
-    Time.timeScale=1f;
-    Domidman.SetActive(false);
-}
 
-    public void AddHealth(){
-        player.HP+=10;
-        Time.timeScale=1f;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
         Domidman.SetActive(false);
     }
-    public void SubHealth(){
-        player.HP-=9;
-        Time.timeScale=1f;
+
+    public void AddHealth()
+    {
+        player.HP += 10;
+        Time.timeScale = 1f;
+        Domidman.SetActive(false);
+    }
+    public void SubHealth()
+    {
+        player.HP -= 9;
+        Time.timeScale = 1f;
 
         Domidman.SetActive(false);
     }
-    
+
 
 
 }
--- Assets/Scripts/Trash.cs
+++ Assets/Scripts/Trash.cs
@@ -9,31 +9,14 @@ public class Trash : MonoBehaviour
 
     void Start()
     {
-    Random random = new Random();
-    num1= random.Next(-1,2);
-    num2= random.Next(-12, -6);
-        // Debug.Log(num1);
-        // Debug.Log(num2);
+  
     }
 
 
     void Update()
     {
-        Move();
-
-        if(transform.position.x <-40){
-            Destroy(gameObject);
-        }
+  
     }
 
-    void Move(){
-        int moveSpeed=num1;
-        int trash_Level=num2;
-
-        Vector2 newPos=new Vector2();
-
-        newPos.x=transform.position.x + num2 * Time.deltaTime;
-        newPos.y=num1 * 7;
-        transform.position=newPos;
-    }
+ 
 }
