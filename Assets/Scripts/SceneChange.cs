fdad4f9a51736ca44fd40f689ddd2491ae2ae273
정은수 <80330331+intsoo@users.noreply.github.com>
2023-01-08 04:45:43 +0900

    UI

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// UI
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public static SceneChange instance;
+
+    // Game States
+    // 1) Game Over
+    private bool isGameOver;
+    // 2) Game Play: Fever Time
+    private bool isFeverTime;
+
+
+    void Awake()
+    {
+        if(!isGameOver) {
+            instance=this;
+        }
+    }
+
+    void Start()
+    {
+        isGameOver = false;
+        isFeverTime = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+       // Change scene
+    public void playGame() {
+        isGameOver = false;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void showMenu(){
+        isGameOver = true;
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void quitGame(){
+        Debug.Log("Game Over");
+        Application.Quit();
+    }
+
+}
