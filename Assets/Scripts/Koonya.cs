b9380afd56404c9f72210832bde41c21c3697a3f
kshyun0415 <71251902+kshyun0415@users.noreply.github.com>
2023-01-08 12:47:33 +0900

    Koongya Level

---
+++ Assets/Scripts/Koonya.cs
@@ -0,0 +1,24 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Koonya : MonoBehaviour
+{
+    public Player player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    // Update is called once per frame
+    
+
+}
+void Update()
+    {
+    //    Vector3 temp= transform.position;
+    //    temp.x =(float) player.HP*(-1);
+
+    //    transform.position=temp;
+     }
+}
